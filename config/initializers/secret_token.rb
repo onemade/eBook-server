1cf887d7b219f757c9e03179313bb16018aef0d1
onemade <onemade@ubuntu.(none)>
2012-12-24 19:26:55 +0800

    rails new eBook-server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EBookServer::Application.config.secret_token = '1ef8acb375edf6ef8e460333059d6548b56e85b3702ecb7498044ee5b3f2276a4c1df2cc85ba2241c2bdf90941918efd60b9c048cb019fe56f177482a3a786d7'
